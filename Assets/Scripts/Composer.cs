b82b00ac44b8ddfc7309983ebeb7ae9d8747ead7
Ezra Marks <43857686+EzraMarks@users.noreply.github.com>
2019-10-19 13:54:58 -0400

    Initial Commit

---
+++ Assets/Scripts/Composer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Composer : MonoBehaviour
+{
+    //Song beats per minute
+    public float songBPM;
+
+    //Number of seconds per song beat
+    public float secPerBeat;
+
+    //Current song position in seconds
+    public float songPosition;
+
+    //Current song position in beats
+    public float songPositionInBeats;
+
+    //Total time in seconds since song started
+    public float dspSongTime;
+
+    //audio source attached to object to play music
+    public AudioSource musicSource;
+
+    //Adjustable offset for first beat of song
+    public float firstBeatOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Load the AudioSource attached to the Conductor
+        musicSource = GetComponent<AudioSource>();
+
+        //Find seconds per song beat
+        secPerBeat = 60f / songBPM;
+
+        //Record when the music starts
+        dspSongTime = (float)AudioSettings.dspTime;
+
+        //Start playing the music
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //find how many seconds since the song started
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
+
+        //find how many beats since the song started
+        songPositionInBeats = songPosition / secPerBeat;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public float moveDistance = 10;
+    public int numberOfLanes = 3;
+    private int playerPosition = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if up key is pressed
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (playerPosition < (numberOfLanes - 1))
+            {
+                // move player up on y axis
+                transform.Translate(0, moveDistance, 0);
+                // increment the player lane
+                playerPosition++;
+            }
+        }
+        // if down key is pressed
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (playerPosition > 0)
+            {
+                // move player down on y axis
+                transform.Translate(0, -moveDistance, 0);
+                // decrement the player lane
+                playerPosition--;
+            }
+        }
+    }
+}
