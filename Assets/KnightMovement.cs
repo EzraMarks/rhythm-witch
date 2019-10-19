97320b1a61020ae00a624580ddd9e7cbc9d60c8a
tobybartclev <56485540+tobybartclev@users.noreply.github.com>
2019-10-19 14:54:54 -0400

    First Enemy Movement Script Created

---
+++ Assets/KnightMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightMovement : MonoBehaviour
+{
+
+    public float KnightSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        KnightSpeed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
