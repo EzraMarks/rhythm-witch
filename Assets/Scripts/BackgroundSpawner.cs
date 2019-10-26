9d124db245cd6facb7e217f0bfd6417747d9aa08
Ezra Marks <43857686+EzraMarks@users.noreply.github.com>
2019-10-26 15:29:28 -0400

    Add paralax background

---
+++ Assets/Scripts/BackgroundSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSpawner : MonoBehaviour
+{
+
+    private double nextActionTime = 0.0;
+    public double backgroundSpawnPeriod = 1;
+
+    public GameObject layer0;
+    public GameObject layer1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += backgroundSpawnPeriod;
+            Instantiate(layer0);
+            Instantiate(layer1);
+        }
+    }
+}
---
+++ Assets/Scripts/ParalaxBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxBackground : MonoBehaviour
+{
+    public float layer0Speed = 1;
+    public float layer1Speed = 2;
+
+    public GameObject layer0;
+    public GameObject layer1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
