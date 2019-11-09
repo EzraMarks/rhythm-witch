d3bf9943accf95f0615ec86d633c51844a92ebc9
Ezra Marks <43857686+EzraMarks@users.noreply.github.com>
2019-11-09 16:20:44 -0500

    Create boss spawning functionality

---
+++ Assets/Scripts/Enemy Movement/Boss1Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1Movement : MonoBehaviour
+{
+    public GameObject nextBossPhase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnDestroy()
+    {
+        // creating a local game object to store the instantiated object and then casting it to a Gamebject
+        nextBossPhase = Instantiate(nextBossPhase) as GameObject;
+
+        // Setting the position of the prefab
+        nextBossPhase.transform.position =
+            new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+}
--- Assets/Scripts/Enemy Movement/Boss3Movement.cs
+++ Assets/Scripts/Enemy Movement/Boss3Movement.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss1 : MonoBehaviour
+public class Boss3Movement : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnDestroy()
+    {
+
     }
 }
