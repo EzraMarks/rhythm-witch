e3ff7f22614fa35969b3ed8a817989bde04e437c
Ezra Marks <43857686+EzraMarks@users.noreply.github.com>
2019-11-09 16:25:32 -0500

    Update Boss3Movement.cs

--- Assets/Scripts/Enemy Movement/Boss3Movement.cs
+++ Assets/Scripts/Enemy Movement/Boss3Movement.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Boss3Movement : MonoBehaviour
 {
@@ -19,6 +21,6 @@ public class Boss3Movement : MonoBehaviour
 
     private void OnDestroy()
     {
-
+        SceneManager.LoadScene("VictoryScene");
     }
 }
