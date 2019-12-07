c6c18725053194bd77e14eeff4fe8cdf2695650e
tobybartclev <56485540+tobybartclev@users.noreply.github.com>
2019-12-07 18:38:47 -0500

    Created credits scene (buggy)

--- Assets/Scripts/CreditsButtons.cs
+++ Assets/Scripts/CreditsButtons.cs
@@ -3,52 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class CreditsButtons : MonoBehaviour
 {
 
     //Background object for sound effect script references
     public WitchSFX WitchSFX;
-    //Main menu music object
-    public GameObject menumusic;
 
     // Start is called before the first frame update
     void Start()
     {
         //Find the background spawner object to assign the SFX script
         WitchSFX = GameObject.Find("BackgroundSpawner").GetComponent<WitchSFX>();
-        //Find the menu music object
-        menumusic = GameObject.Find("MenuMusic");
     }
 
+    
     // Update is called once per frame
     void Update()
     {
         
     }
 
-    public void GameStart()
-    {
-        Destroy(menumusic);
-        WitchSFX.StartButton();
-        SceneManager.LoadScene("LevelScene");
-    }
-
-    public void Instructions()
-    {
-        WitchSFX.ButtonForward();
-        SceneManager.LoadScene("InstructionsScene");
-    }
-
-    public void Credits()
-    {
-        WitchSFX.ButtonForward();
-        SceneManager.LoadScene("CreditsScene");
-    }
-
-    public void GameQuit()
+    public void MainMenu()
     {
         WitchSFX.ButtonBackward();
-        Application.Quit();
+        SceneManager.LoadScene("MenuScene");
     }
 
     public void MouseOver()
